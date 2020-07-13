99fc8db5e333fd3da52fcb2e570e6d00984dc20f
metallfield <didjok6@gmail.com>
2020-07-13 09:04:43 +0300

    trying js

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -7,7 +7,7 @@
     <form action="{{route('post.store')}}" method="post" enctype="multipart/form-data">
         <div class="form-group">
             <label for="name">name</label>
-            <input type="text" name="name" class="form-control" required>
+            <input type="text" name="name" class="form-control" >
         </div>
         @error('name')
         <p class="alert alert-warning">{{$message}}</p>
@@ -19,13 +19,24 @@
         @error('content')
         <p class="alert alert-warning">{{$message}}</p>
         @enderror
+        <div class="ui-widget">
+            <label for="tags">Tags </label>
+            <input id="tags" size="50">
+        </div>
       <div class="form-group">
-          <label for="tags">tags</label>
-          <input type="text" name="tags" >
+           <select name="tags[]" id="tag" multiple="multiple" class="form-control" >
+              @foreach($tags as $tag)
+              <option >{{$tag->name}}</option>
+                  @endforeach
+          </select>
       </div>
         @error('tags')
         <p class="alert alert-warning">{{$message}}</p>
         @enderror
+        <div class="custom-control custom-switch">
+            <input type="checkbox" class="custom-control-input " id="customSwitch1" >
+            <label class="custom-control-label" for="customSwitch1">has image</label>
+        </div>
         <div class="form-group">
             <input id="image" type="file" class="form-controll-file" name="image" >
         </div>
@@ -36,4 +47,5 @@
         @csrf
     </form>
 </div>
+
 @endsection
--- resources/views/layouts/base_layout.blade.php
+++ resources/views/layouts/base_layout.blade.php
@@ -69,5 +69,67 @@
 </header>
 @yield('content')
 <script src="{{ mix('js/app.js') }}"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<script>
+    jQuery(function () {
+        $('#image').hide();
+        $('#customSwitch1').click(function () {
+           $('#image').fadeToggle(1000  );
+        });
+        $('select#tag').hide();
+        $('select#tags').css({'border':'none'});
+
+
+        $( function() {
+            var availableTags = [];
+            $('select#tag option').each(function () {
+                availableTags.push($(this).val()) ;
+            });
+            function split( val ) {
+                return val.split( /,\s*/ );
+            }
+            function extractLast( term ) {
+                return split( term ).pop();
+            }
+
+            $( "#tags" )
+            // don't navigate away from the field on tab when selecting an item
+                .on( "keydown", function( event ) {
+                    if ( event.keyCode === $.ui.keyCode.TAB &&
+                        $( this ).autocomplete( "instance" ).menu.active ) {
+                        event.preventDefault();
+                    }
+                })
+                .autocomplete({
+                    minLength: 0,
+                    source: function( request, response ) {
+                        // delegate back to autocomplete, but extract the last term
+                        response( $.ui.autocomplete.filter(
+                            availableTags, extractLast( request.term ) ) );
+                    },
+                    focus: function() {
+                        // prevent value inserted on focus
+                        return false;
+                    },
+                    select: function( event, ui ) {
+                        var terms = split( this.value );
+                        // remove the current input
+                        terms.pop();
+                        // add the selected item
+                        terms.push( ui.item.value );
+                        // add placeholder to get the comma-and-space at the end
+                        terms.push( "" );
+                        this.value = terms.join( ", " );
+                        return false;
+                    }
+                });
+        } );
+    })
+</script>
+
+
 </body>
 </html>
